6d7bd3925f75ad529ce5d7a6a003a2d57fc2e039
ashashrith <ashrith266@gmail.com>
2023-05-23 05:40:33 +0000

    coding-practise-4

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "cricketTeam.db");
+let db = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3005, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+let convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+app.get("/players/", async (request, response) => {
+  const listOfPlayers = `SELECT * FROM cricket_team`;
+  const playersArray = await db.all(listOfPlayers);
+  response.send(
+    playersArray.map((eachPlayer) => convertDbToResponseObject(eachPlayer))
+  );
+});
+
+module.exports = app;
